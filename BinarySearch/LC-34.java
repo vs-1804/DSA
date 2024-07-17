38f2609674ceaa2cc7beef4c4d4e938d042addeb
vikas <65765933+vs-1804@users.noreply.github.com>
2024-07-17 10:59:46 +0530

    Update LC-34.java

--- BinarySearch/LC-34.java
+++ BinarySearch/LC-34.java
@@ -1,1 +1,44 @@
 //34. Find First and Last Position of Element in Sorted Array
+class Solution {
+    public int lowerBinary(int[] arr, int target) {
+        int ans=-1;int left=0;
+        int right=arr.length-1;
+    	while(left<=right) {
+			int mid=left+(right-left)/2;
+			if(arr[mid]==target) {
+				ans=mid;
+				right=mid-1;
+			}
+			else if(arr[mid]>=target) {
+				right=mid-1;
+			}
+			else {
+				left=mid+1;
+			}
+		}
+	return ans;
+    }
+    	public  int upperBinary(int[] arr, int target) {
+		int left = 0; int ans=-1;
+		int right = arr.length - 1;
+			while (left <= right) {
+			int mid=left+(right-left)/2;
+			if(arr[mid]==target) {
+				ans=mid;
+				left=mid+1;
+			}
+			else if(arr[mid]<target) {
+				left=mid+1;
+			}
+			else{
+				right=mid-1;
+			}
+		}
+		return ans;
+	}
+    public int[] searchRange(int[] nums, int target) {
+         int a=lowerBinary( nums, target);
+         int y= upperBinary(nums,  target);
+           return new int[]{a,y};
+    }
+}
