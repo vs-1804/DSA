425886839df3bdbe4cca8d58a0750f99fd6181e4
vikas <65765933+vs-1804@users.noreply.github.com>
2024-09-25 15:19:32 +0530

    Update QUEST7.java

--- Recurssion/QUEST7.java
+++ Recurssion/QUEST7.java
@@ -3,7 +3,7 @@ package com.java;
 import java.util.ArrayList;
 import java.util.List;
 
-public class QUEST7 {
+public class QUEST7 {/// generate all permutation without extra space
 	public static void main(String[] args) {
 		int[] arr= {1,2,3};
 		List<List<Integer>> ans = new ArrayList<List<Integer>>();
